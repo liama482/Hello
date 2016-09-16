abf52a45b83d6fa6d0d483b9fd51e6d3af991b40
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-16 13:37:43 -0400

    Updated from Brython Server: 9/16/2016 1:37:43 PM

--- Conditional_State.py
+++ Conditional_State.py
@@ -1,3 +1,4 @@
+"""
 temperature = float(input("What is the temperature outside? "))
 weather = input("Is it raining or sunny outside? ")
 weather = weather.lower() # change to lower case
@@ -6,3 +7,10 @@ if temperature < 55:
 else:  # 55 or warmer..
     clothes = "an umbrella" if weather == "raining" else "sunglasses"
 print("You should bring {0}.".format(clothes))
+"""
+
+temperatures = [12, 46, 24, 67, 80, 26, 64, 24, 45, 40]
+howitfeels = ["{0} is Brr!".format(temp) if temp < 50 else temp for temp in temperatures]
+print(howitfeels)
+#['12 is Brr!', 46, '24 is Brr!', 67, 80, '26 is Brr!', 64, '24 is Brr!', 45, 40]
+print(temp)
